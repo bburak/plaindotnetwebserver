97ba13a4044be5ba72000c8f22d683fbdcb388e5
Burak Bilgic <bilgicburakb@gmail.com>
2023-01-02 15:49:31 +0300

    changes for .net6 project

--- WebServerDotNet5/Program.cs
+++ WebServerDotNet5/Program.cs
@@ -3,10 +3,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Hosting;
 
-
+// HTTP request comes in it should run this method.
+// Whenever HTTP request comes in we answer with Hello World.
 Host.CreateDefaultBuilder(args)
-    // HTTP request comes in it should run this method.
-    // Whenever HTTP request comes in we answer with Hello World.
     .ConfigureWebHostDefaults(webBuilder =>
     {
         webBuilder.Configure(app =>
